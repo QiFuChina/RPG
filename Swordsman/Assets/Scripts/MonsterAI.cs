38c83459e5c94ba967537902959789866263db95
QiFuChina <517469138@qq.com>
2018-03-13 23:20:25 +0000

    battle with monster, monster die and disappear

--- Swordsman/Assets/Scripts/MonsterAI.cs
+++ Swordsman/Assets/Scripts/MonsterAI.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonsterAI : MonoBehaviour {
 
+	public int hp;
+	[Range(0,10)]
+	public int hpMax=10; 
+
+	public Text hpText;
+	public Slider hpSlider;
+
+	private Animator anim;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+		hp=hpMax;
+		anim=GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		hp=Mathf.Clamp(hp,0,10);
+		hpText.text=hp+"/"+hpMax;
+		hpSlider.value=(float)hp/(float)hpMax;
+		if(hp<=0){
+			Die();
+		}
 	}
-	void OnTriggerEnter(Collider col)
-	{}
+	void OnTriggerEnter(Collider col){
+		hp-=1;
+	}
+	void Die(){
+		anim.SetTrigger("die");
+		Destroy(gameObject);
+	}
 }
